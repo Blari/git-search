81afe6cf9e0c974d69b0d846ccfee4927f8ee778
Blari <Blarior@gmail.com>
2020-03-29 18:53:29 +0300

    Updates

---
+++ precache-manifest.1c4e73422e0c90e1da8a6724ff89cb76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02c4bf77ead81a42348da984cd67ad4",
+    "url": "/git-search/index.html"
+  },
+  {
+    "revision": "a14f27eb542ffddf7537",
+    "url": "/git-search/static/css/main.3114cb68.chunk.css"
+  },
+  {
+    "revision": "1b85f75d495f46b4186a",
+    "url": "/git-search/static/js/2.b6c82661.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/git-search/static/js/2.b6c82661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a14f27eb542ffddf7537",
+    "url": "/git-search/static/js/main.1cf350f2.chunk.js"
+  },
+  {
+    "revision": "84463662581b7e7b9d5b",
+    "url": "/git-search/static/js/runtime-main.017d07dd.js"
+  }
+]);
